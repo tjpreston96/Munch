062b2d217d654a582ba2fc99b4a68992017e61e0
Juliors1 <user@email.com>
2021-01-14 16:46:09 -0500

    Box shadow on user card

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -15,7 +15,7 @@ class Users extends Component {
     return (
       <>
         <div className='userCard'>
-        <h1>Hello. This is a list of all the users.</h1>
+        <h1>User List:</h1>
         {this.state.users.map((user) => (
             <p>{user.name} </p>
 
