f156c915b622e0f94a6b3eb500b0d603a2692c92
tpreston96 <tpreston96@gmail.com>
2021-01-15 21:31:54 -0600

    friday progress

--- src/services/recipe-api.js
+++ src/services/recipe-api.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://api.edamam.com/'
 
 
 export function recipeSearch(formData){
-    return fetch(`${BASE_URL}/search?q=${formData.query}`, {
+    return fetch(`${BASE_URL}/search?q=${formData.query}&${process.env.APP_ID}&${process.env.APP_KEY}`, {
         method: "GET",
           headers: {'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken()},
           body: JSON.stringify(formData)
