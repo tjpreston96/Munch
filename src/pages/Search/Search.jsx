459e28f18068d953f0fc7ba78317a4280311f6bf
tpreston96 <tpreston96@gmail.com>
2021-01-15 19:46:36 -0600

    app route fixes

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,21 +1,49 @@
 import React, { Component } from "react";
 import "./Search.css";
+import * as recipeAPI from "../../services/recipe-api";
 
 class Search extends Component {
   state = {
-    results: [],
+    recipes: [],
     formData: {
       query: "",
     },
   };
+
+  handleSubmit = (e) => {
+    // prevents automatic refresh before submit
+    e.preventDefault();
+    this.handleSearch(this.state.formData);
+  };
+
+  handleChange = (e) => {
+    // setting formData query string to the input from user, then passed to handleSearch
+    const formData = {
+      ...this.state.formData,
+      [e.target.name]: e.target.value,
+    };
+    this.setState({ formData: formData });
+  };
+
+  handleSearch = async (formData) => {
+    const recipes = await recipeAPI.recipeSearch(formData)
+  };
+
   render() {
     return (
       <>
         <div className="searchCard">
           <h2>Find a Recipe</h2>
           <div>
-            <input type="text" />
-            <button type="submit">Search</button>
+            <form>
+              <input
+                type="text"
+                name="query"
+                value={this.state.formData.query}
+                onChange={this.handleChange}
+              />
+              <button type="submit">Search</button>
+            </form>
           </div>
         </div>
       </>
