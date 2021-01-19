8e39814feba8fe230fd8b3c3e5c6a826064bd096
Mbanks1 <michael1banks@icloud.com>
2021-01-19 12:45:27 -0400

    board crud

--- controllers/board.js
+++ controllers/board.js
@@ -1,29 +1,49 @@
 const Recipe = require('../models/board');
 
-
 module.exports = {
     index,
     create,
     show,
-    reply
-}
+    reply,
+    delete: deleteOne,
+    deleteReply
+};
+
+function deleteReply(req, res){
+    Recipe.findById(req.params.messageId)
+    .then((message) => {res.json(message)})
+    .catch((err) => {res.json(err)})
+};
 
 function index(req, res) {
-    Recipe.find({})
-    .then((recipes) => {
-    res.json(recipes);
-    });
-    }
+ Recipe.find({})
+ .then((recipes) => {res.json(recipes)})
+ .catch((err) => {res.json(err)})
+ };
 
 function create(req, res){
-
+  Recipe.create(req.body)
+ .then(recipe => {res.json(recipe)})
+ .catch(err => {res.json(err)})
 };
 
 function show(req, res){
-
+    Recipe.findById(req.params.id)
+    .then((recipe) => {res.json(recipe)})
+    .catch((err) => {res.json(err)})
 };
 
 function reply(req, res){
-
+    Recipe.findById(req.params.id)
+    .then((post) => {post.replies.push(req.body)
+    post.save()
+    .then(() => res.json(post._id))})
+    .catch((err) => {res.json(err)})
 };
+
+function deleteOne(req,res){
+    Recipe.findByIdAndDelete(req.params.id)
+    .then(post => {res.json(post)})
+    .catch((err) => {res.json(err)})
+}
 
