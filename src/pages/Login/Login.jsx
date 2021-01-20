e82660bad31eff4122304492e237dcac1b16f313
tpreston96 <tpreston96@gmail.com>
2021-01-20 15:10:16 -0600

    login refactor

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -36,32 +36,36 @@ class LoginPage extends Component {
     const { email, pw } = this.state;
     return (
       <main className="Login">
-        <h3>Log In</h3>
-        <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            autoComplete="off"
-            id="email"
-            value={email}
-            name="email"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="password"
-            autoComplete="off"
-            id="password"
-            value={pw}
-            name="pw"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <button className="btn green">Log In</button>&nbsp;&nbsp;&nbsp;
-          <Link className="btn red" to="/">
-            Cancel
-          </Link>
-        </form>
-        <p>{this.state.message}</p>
+        <div className="loginPage">
+          <h3>Log In</h3>
+          <form autoComplete="off" onSubmit={this.handleSubmit}>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="text"
+              autoComplete="off"
+              id="email"
+              value={email}
+              name="email"
+              onChange={this.handleChange}
+            />
+            <br />
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              autoComplete="off"
+              id="password"
+              value={pw}
+              name="pw"
+              onChange={this.handleChange}
+            />
+            <br />
+            <button className="btn btn-info">Log In</button>&nbsp;&nbsp;&nbsp;
+            <Link className="btn btn-secondary" to="/">
+              Cancel
+            </Link>
+          </form>
+          <p>{this.state.message}</p>
+        </div>
       </main>
     );
   }
