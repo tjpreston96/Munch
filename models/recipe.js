be4d49415768b66163fc904d1fde8aa2fb8cdb58
tpreston96 <tpreston96@gmail.com>
2021-01-28 19:25:39 -0600

    model change

--- models/recipe.js
+++ models/recipe.js
@@ -15,23 +15,26 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema(
   {
-    title: { type: String},
-    ingredients: { type: [Schema.Types.Mixed]},
-    recipe: { type: String},
-    label: { type: String},
-    image: { type: String},
-    source: { type: String},
-    url: { type: String},
-    yield: { type: Number},
-    healthLabels: { type: [String]},
-    calories: { type: Number},
-    totalTime: { type: Number},
-    totalNutrients: { type: Schema.Types.Mixed},
-
+    title: { type: String },
+    ingredients: { type: [Schema.Types.Mixed] },
+    recipe: { type: String },
+    label: { type: String },
+    image: { type: String },
+    source: { type: String },
+    url: { type: String },
+    yield: { type: Number },
+    healthLabels: { type: [String] },
+    calories: { type: Number },
+    totalTime: { type: Number },
+    totalNutrients: { type: Schema.Types.Mixed },
+    digest: { type: Schema.Types.Mixed },
   },
   {
     timestamps: true,
   }
 );
 
-module.exports = {recipeSchema,model:mongoose.model("Recipe", recipeSchema)}
+module.exports = {
+  recipeSchema,
+  model: mongoose.model("Recipe", recipeSchema),
+};
